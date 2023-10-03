1db3c5add8822ab95c9d52b4508020663bc4d8fd
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2023-10-03 11:25:31 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,35 +1,9 @@
-#include <iostream>
-#include <boost/filesystem/operations.hpp>
+#include "bayan.h"
 
 #include "lib.h"
 
-int main(int, char **) {
-    boost::filesystem::directory_iterator begin("./");
-    boost::filesystem::directory_iterator end; 
-        for (; begin != end; ++ begin) {
-            boost::filesystem::file_status fs = 
-            boost::filesystem::status(*begin); {
-                switch (fs.type()) {
-                    case boost::filesystem::regular_file: 
-                        std::cout << "FILE ";
-                        break;
-                    case boost::filesystem::symlink_file:
-                        std::cout << "SYMLINK ";
-                        break;
-                    case boost::filesystem::directory_file: 
-                        std::cout << "DIRECTORY ";
-                        break;
-                    default: 
-                        std::cout << "OTHER ";
-                        break;
-                    }
-                if (fs.permissions() & boost::filesystem::owner_write) {
-                        std::cout << "W ";
-                    } else {
-                        std::cout << " ";
-                    }
-            }
-        std::cout << *begin << '\n';
-        } /*for*/
+int main(int argc, char ** argv) {
+    bayan::Bayan instance;
+    instance.getDublicates(argc, argv); 
     return 0;
-} /*main*/
+}
